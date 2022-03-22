e1f811acff9a24b8cc8bba7960f21792be99e3b4
shamilkeheliya <shamilkeheliya@gmail.com>
2022-03-22 15:38:00 +0530

    Update LoadingScreen.kt

--- App/app/src/main/java/com/codezen/healthcare/LoadingScreen.kt
+++ App/app/src/main/java/com/codezen/healthcare/LoadingScreen.kt
@@ -1,21 +1,23 @@
 package com.codezen.healthcare
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 private val SPLASH_TIME_OUT:Long = 3000
 class LoadingScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {}
+
         setContentView(R.layout.activity_loading_screen)
 
         Handler().postDelayed({
-
-            startActivity(Intent(this,Home::class.java))
-
-            // close this activity
+            startActivity(Intent(this, Home::class.java))
             finish()
         }, SPLASH_TIME_OUT)
     }
