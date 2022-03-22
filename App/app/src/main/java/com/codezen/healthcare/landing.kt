4b6a7400ddf98a0b19a9703b0c75df1285bdf40d
Hashini99 <bhhrkumari@students.nsbm.ac.lk>
2022-03-22 11:08:52 +0530

    create landing

---
+++ App/app/src/main/java/com/codezen/healthcare/landing.kt
@@ -0,0 +1,27 @@
+package com.codezen.healthcare
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class landing :AppCompatActivity(){
+    // This is the loading time of the splash screen
+    private val SPLASH_TIME_OUT:Long = 3000 // 1 sec
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this,MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
+
