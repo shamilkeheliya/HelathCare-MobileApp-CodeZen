5930835aee19477fd700b9bf411d812f78ce17ae
Hashini99 <bhhrkumari@students.nsbm.ac.lk>
2022-03-27 19:19:03 +0530

    create authentication

--- App/app/src/main/java/com/codezen/healthcare/Login.kt
+++ App/app/src/main/java/com/codezen/healthcare/Login.kt
@@ -1,11 +1,37 @@
 package com.codezen.healthcare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
+    private lateinit var  auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        title="Login"
+        auth= FirebaseAuth.getInstance()
+    }
+    fun login(view: View){
+        val email=editTextEmail.text.toString()
+        val password=editTextPassword.text.toString()
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                val intent= Intent(this,NewOrder::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener { exception ->
+            Toast.makeText(applicationContext,exception.localizedMessage, Toast.LENGTH_LONG).show()
+        }
+    }
+    fun goToRegister(view: View){
+        val intent= Intent(this,CreateAccount::class.java)
+        startActivity(intent)
     }
 }
