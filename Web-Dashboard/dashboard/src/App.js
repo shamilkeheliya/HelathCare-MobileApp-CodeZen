8526d6e48caa12b79f14513c531f58a6e1ca4dad
shamilkeheliya <shamilkeheliya@gmail.com>
2022-04-10 18:49:04 +0530

    Firebase config done

--- Web-Dashboard/dashboard/src/App.js
+++ Web-Dashboard/dashboard/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './firebase/config';
 
 function App() {
   return (
---
+++ Web-Dashboard/dashboard/src/firebase/config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD9iyn3qdZQ2YzXvjgvoEs_HMmiupVWiK8",
+    authDomain: "health-care---codezen.firebaseapp.com",
+    projectId: "health-care---codezen",
+    storageBucket: "health-care---codezen.appspot.com",
+    messagingSenderId: "86394190095",
+    appId: "1:86394190095:web:c4f9b07183b53fe21da67a",
+    measurementId: "G-5H5MZJQS6C"
+};
+
+const app = initializeApp(firebaseConfig);
+
+console.log(app().log);
