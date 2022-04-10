7acfc9d3a86d1f711df3b45dadf04c9658ae3d28
Devindi Rajapakse <71745965+devindirajapakse@users.noreply.github.com>
2022-04-10 14:32:02 +0530

    Update Profile.kt

--- App/app/src/main/java/com/codezen/healthcare/Profile.kt
+++ App/app/src/main/java/com/codezen/healthcare/Profile.kt
@@ -3,16 +3,27 @@ package com.codezen.healthcare
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {}
+
         setContentView(R.layout.activity_profile)
     }
 
     override fun onBackPressed() {
         super.onBackPressed()
         startActivity(Intent(this, Orders::class.java))
+        finish()
+    }
+
+    fun goToUpdateProfile(view: View){
+        startActivity(Intent(this, UpdateProfile::class.java))
         finish()
     }
 }
