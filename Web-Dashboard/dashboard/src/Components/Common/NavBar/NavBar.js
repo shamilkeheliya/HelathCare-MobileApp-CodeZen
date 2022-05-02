e1e0f5a51dd5aaab0180bdb52542ad58e8d52dc0
NishuDissanayake <nishudissanayake5252@gmail.com>
2022-05-02 22:21:46 +0530

    Routing completed

--- Web-Dashboard/dashboard/src/Components/Common/NavBar/NavBar.js
+++ Web-Dashboard/dashboard/src/Components/Common/NavBar/NavBar.js
@@ -17,7 +17,7 @@ import {
     MDBDropdownItem,
     MDBDropdownLink
 } from 'mdb-react-ui-kit';
-
+import { NavLink } from 'react-router-dom';
 import { CgProfile } from 'react-icons/cg';
 
 function NavBar() {
@@ -39,12 +39,9 @@ function NavBar() {
                 <MDBCollapse navbar show={showNavText}>
                     <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
                         <MDBNavbarItem>
-                            <MDBNavbarLink active aria-current='page' href='#'>
-                                Home
-                            </MDBNavbarLink>
-                        </MDBNavbarItem>
-                        <MDBNavbarItem>
-                            <MDBNavbarLink href='#'>Order History</MDBNavbarLink>
+                            <NavLink active aria-current='page' to='/orders' className='navlink'>
+                                Order History
+                            </NavLink>
                         </MDBNavbarItem>
                         <MDBNavbarItem>
                             <MDBNavbarLink href='#'></MDBNavbarLink>
@@ -53,9 +50,6 @@ function NavBar() {
                     <MDBDropdown group className='shadow-0'>
                         <MDBDropdownToggle color='light'><span className='navbar-text'>< CgProfile /></span></MDBDropdownToggle>
                         <MDBDropdownMenu>
-                            <MDBDropdownItem>
-                                <MDBDropdownLink href="#">View Profile</MDBDropdownLink>
-                            </MDBDropdownItem>
                             <MDBDropdownItem>
                                 <MDBDropdownLink href="#">Log Out</MDBDropdownLink>
                             </MDBDropdownItem>
